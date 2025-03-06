e7187629c49d26ea43bd6b30532a02cbbabcf0bd
MidnightExcursion <dima.watkins@gmail.com>
2025-03-06 15:13:53 -0600

    compiled global model

--- networks/global_fit/global_fit_model.py
+++ networks/global_fit/global_fit_model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Concatenate, Dense, Lambda
+from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
 
 def global_fit_model():
@@ -9,16 +9,26 @@ def global_fit_model():
         maxval = 10.0,
         seed = None)
 
-    circle_function_input = Input(shape=(3,), name = "spherical_function_inputs")
+    input_kinematics = Input(shape=(3,), name = "input_kinematics")
 
-    x1 = tf.keras.layers.Dense(480, activation="relu6", kernel_initializer=initializer)(circle_function_input)
+    x1 = tf.keras.layers.Dense(480, activation="relu6", kernel_initializer=initializer)(input_kinematics)
     x2 = tf.keras.layers.Dense(320, activation="relu6", kernel_initializer=initializer)(x1)
     x3 = tf.keras.layers.Dense(240, activation="relu6", kernel_initializer=initializer)(x2)
     x4 = tf.keras.layers.Dense(120, activation="relu6", kernel_initializer=initializer)(x3)
     x5 = tf.keras.layers.Dense(32, activation="relu6", kernel_initializer=initializer)(x4)
 
-    output_y_value = Dense(4, activation = "linear", kernel_initializer = initializer, name = "output_radial_value")(x5)
+    output_y_value = Dense(4, activation = "linear", kernel_initializer = initializer, name = "output_global_cff_values")(x5)
 
-    tensorflow_network = Model(inputs = circle_function_input, outputs = output_y_value, name = "spherical_function_fitter")
+    tensorflow_network = Model(
+        inputs = input_kinematics,
+        outputs = output_y_value,
+        name = "global_fit_cffs")
+
+    tensorflow_network.compile(
+            optimizer = 'adam',
+            loss = tf.keras.losses.MeanSquaredError(),
+            metrics = [
+                tf.keras.metrics.MeanSquaredError()
+            ])
 
     return tensorflow_network
